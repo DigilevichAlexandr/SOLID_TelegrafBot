8e2a31efb7ac327907d535fc860d448d7e1d94b8
Digilevich Alexandr <Digilevich.Alexandr@gmail.com>
2015-11-07 13:17:32 +0300

    3

--- become-solid-in-csharp-master/day-1/BecomeSolid.Day1/Program.cs
+++ become-solid-in-csharp-master/day-1/BecomeSolid.Day1/Program.cs
@@ -22,7 +22,7 @@ namespace BecomeSolid.Day1
         static async Task Run()
         {
             var bot = new Api("127266371:AAHoh0eh4Nt0mzB1LOoZwg8E7vQqeoMswuI");
-            var weatherApiKey = "ec259b32688dc1c1d087ebc30cbff9ed";
+            
             var me = await bot.GetMe();
 
             Console.WriteLine("Hello my name is {0}", me.Username);
@@ -42,9 +42,10 @@ namespace BecomeSolid.Day1
                         if (inputMessage.StartsWith("/currency"))
                         {
                             var messageParts = inputMessage.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                            var city = messageParts.Length == 1 ? "Minsk" : messageParts.Skip(1).First();
-                            WebUtility.UrlEncode(city);
-                            string url = string.Format("https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=", city, weatherApiKey);
+                            var kindCurrency = messageParts.Length == 1 ? "USDBYR" : messageParts.Skip(1).First();
+                            WebUtility.UrlEncode(kindCurrency);
+                            
+                            string url = string.Format("https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22{0}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=", kindCurrency);
                             WebRequest request = WebRequest.Create(url);
                             WebResponse response = request.GetResponse();
                             using (var streamReader = new StreamReader(response.GetResponseStream()))
@@ -55,18 +56,18 @@ namespace BecomeSolid.Day1
                                 JObject joResponse = JObject.Parse(responseString);
                                 JObject query = (JObject)joResponse["query"];
                                 JObject results = (JObject)query["results"];
-                                JObject firstRate = (JObject)results["rate"][0];
-                                string USDRUB_Id = (string)firstRate["Id"];
-                                string Name = (string)firstRate["Name"];
-                                string Rate = (string)firstRate["Rate"];
-                                string Date = (string)firstRate["Date"];
-                                string Time = (string)firstRate["Time"];
-                                string Ask = (string)firstRate["Ask"];
-                                string Bid = (string)firstRate["Bid"];
+                                JObject rateObj = (JObject)results["rate"];
+                                string id = (string)rateObj["Id"];
+                                string name = (string)rateObj["Name"];
+                                string rate = (string)rateObj["Rate"];
+                                string date = (string)rateObj["Date"];
+                                string time = (string)rateObj["Time"];
+                                string ask = (string)rateObj["Ask"];
+                                string bid = (string)rateObj["Bid"];
 
-                                Console.WriteLine(string.Format("{0} is: {1} ", USDRUB_Id, Rate));
+                                Console.WriteLine(string.Format("{0} is: {1} ", id, rate));
 
-                                var message = "Курс " + Name + "Дата " + Date + "Время " + Time + "Покупка " + Ask + "Продажа " + Bid;
+                                var message = "Курс " + name + "Дата " +date + "Время " + time + "Покупка " + ask + "Продажа " + bid;
 
                                 var t = await bot.SendTextMessage(update.Message.Chat.Id, message);
                                 Console.WriteLine("Echo Message: {0}", message);
@@ -78,6 +79,7 @@ namespace BecomeSolid.Day1
                             var messageParts = inputMessage.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                             var city = messageParts.Length == 1 ? "Minsk" : messageParts.Skip(1).First();
                             WebUtility.UrlEncode(city);
+                            var weatherApiKey = "ec259b32688dc1c1d087ebc30cbff9ed";
                             string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&APPID={1}&units=metric", city, weatherApiKey);
                             WebRequest request = WebRequest.Create(url);
                             WebResponse response = request.GetResponse();
