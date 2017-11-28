2a05e8a22fb37fc88204b08d94ef47631ea094a9
Alexandr Digilevich <digilevich.alexandr@gmail.com>
2017-11-28 19:56:13 +0300

    recovery for full sql server. new bot criptomoney related colled CriptoMoneyNewsSerferBot CriptoMoneyNewsSerfer_bot

--- become-solid-in-csharp-master/day-1/BecomeSolid.Day1/Program.cs
+++ become-solid-in-csharp-master/day-1/BecomeSolid.Day1/Program.cs
@@ -15,165 +15,167 @@ using BecomeSolid.Day1.BL;
 
 namespace BecomeSolid.Day1
 {
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            IRequestResolve requestResolver = new RequestResolver();
-            Run(requestResolver).Wait();
-        }
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			try {
+				IRequestResolve requestResolver = new RequestResolver();
+				Run(requestResolver).Wait();
+			} 
+			catch (Exception ex) {
 
-        private static async Task Run(IRequestResolve requestResolver)
-        {
-            var bot = new Api("285635729:AAGtYT09IoeFeb9g_-hKSfQ-fAoPyR7RNFM");
+			}
+		}
 
-            var me = await bot.GetMe();
+		private static async Task Run(IRequestResolve requestResolver)
+		{
+			var bot = new Api("454883056:AAFWlNt8oyeQouMasGb9JW824YGGk2c6znA");
 
-            Console.WriteLine("Hello my name is {0}", me.Username);
+			var me = await bot.GetMe();
 
-            var offset = 0;
+			Console.WriteLine("Hello my name is {0}", me.Username);
 
-            while (true)
-            {
-                var updates = await bot.GetUpdates(offset);
+			var offset = 0;
 
-                foreach (var update in updates)
-                {
-                    if (update.Message.Type == MessageType.TextMessage)
-                    {
-                        string responseText = requestResolver.GetResponceText(update.Message.Text, bot, update);
-                        var t = await bot.SendTextMessage(update.Message.Chat.Id, responseText);
-                        Console.WriteLine(responseText);
-                        //requestResolver.GetResponceText(update.Message.Text);
-                    }
-                    
-                    //    var inputMessage = update.Message.Text;
-                    //    if (inputMessage.StartsWith("/task"))
-                    //    {
-                            
-                    //    }
-                    //    //else
-                    //    //if (inputMessage.StartsWith("/learn"))
-                    //    //{
-                    //    //    using (BotContext botContext = new BotContext())
-                    //    //    {
-                    //    //        botContext.Ansvers.Add(new Ansver() { Value = "мои дела хорошо, а твои?" });
-                    //    //        botContext.Ansvers.Add(new Ansver() { Value = "" });
-                    //    //        botContext.Ansvers.Add(new Ansver() { Value = update.Message.Text.Remove(0, 7) });
-                    //    //        botContext.SaveChanges();
-                    //    //        IEnumerable<Ansver> allAnsvers = botContext.Ansvers;
-                    //    //    }
-                    //    //}
-                    //    else
-                    //    if (inputMessage.StartsWith("/ai"))
-                    //    {
-                    //        await bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
-                    //        await Task.Delay(2000);
-                    //        using (BotContext botContext = new BotContext())
-                    //        {
-                    //            string[] tags = update.Message.Text.ToLower().Split();
-                    //            string ansver = "";
-                    //            IEnumerable<Ansver> ansvers = botContext.Ansvers;
-                    //            foreach (var tag in tags)
-                    //            {
-                    //                ansvers.Where(w => w.Value.Contains(tag));
-                    //                if (ansvers.Count() != 0)
-                    //                {
-                    //                    ansver += ansvers.First().Value;
-                    //                    break;
-                    //                }
-                    //            }
+			while (true) {
+				var updates = await bot.GetUpdates(offset);
 
-                    //            if (ansver.Equals(""))
-                    //            {
-                    //                var t = await bot.SendTextMessage(update.Message.Chat.Id, "к такому я не был готов");
-                    //            }
-                    //            else
-                    //            {
-                    //                var t = await bot.SendTextMessage(update.Message.Chat.Id, ansver);
-                    //            }
+				foreach (var update in updates) {
+					if (update.Message.Type == MessageType.TextMessage) {
+						string responseText = requestResolver.GetResponceText(update.Message.Text, bot, update);
+						var t = await bot.SendTextMessage(update.Message.Chat.Id, responseText);
+						Console.WriteLine(responseText);
+						//requestResolver.GetResponceText(update.Message.Text);
+					}
 
-                    //        }
+					//    var inputMessage = update.Message.Text;
+					//    if (inputMessage.StartsWith("/task"))
+					//    {
 
-                    //        Console.WriteLine("Echo Message: {0}", update.Message.Text);
-                    //    }
-                    //    else if (inputMessage.StartsWith("/currency"))
-                    //    {
-                    //        var messageParts = inputMessage.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                    //        var kindCurrency = messageParts.Length == 1 ? "USDBYR" : messageParts.Skip(1).First();
-                    //        WebUtility.UrlEncode(kindCurrency);
+					//    }
+					//    //else
+					//    //if (inputMessage.StartsWith("/learn"))
+					//    //{
+					//    //    using (BotContext botContext = new BotContext())
+					//    //    {
+					//    //        botContext.Ansvers.Add(new Ansver() { Value = "мои дела хорошо, а твои?" });
+					//    //        botContext.Ansvers.Add(new Ansver() { Value = "" });
+					//    //        botContext.Ansvers.Add(new Ansver() { Value = update.Message.Text.Remove(0, 7) });
+					//    //        botContext.SaveChanges();
+					//    //        IEnumerable<Ansver> allAnsvers = botContext.Ansvers;
+					//    //    }
+					//    //}
+					//    else
+					//    if (inputMessage.StartsWith("/ai"))
+					//    {
+					//        await bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
+					//        await Task.Delay(2000);
+					//        using (BotContext botContext = new BotContext())
+					//        {
+					//            string[] tags = update.Message.Text.ToLower().Split();
+					//            string ansver = "";
+					//            IEnumerable<Ansver> ansvers = botContext.Ansvers;
+					//            foreach (var tag in tags)
+					//            {
+					//                ansvers.Where(w => w.Value.Contains(tag));
+					//                if (ansvers.Count() != 0)
+					//                {
+					//                    ansver += ansvers.First().Value;
+					//                    break;
+					//                }
+					//            }
 
-                    //        string url =
-                    //            string.Format(
-                    //                "https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22{0}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=",
-                    //                kindCurrency);
-                    //        WebRequest request = WebRequest.Create(url);
-                    //        WebResponse response = request.GetResponse();
-                    //        using (var streamReader = new StreamReader(response.GetResponseStream()))
-                    //        {
-                    //            string responseString = streamReader.ReadToEnd();
+					//            if (ansver.Equals(""))
+					//            {
+					//                var t = await bot.SendTextMessage(update.Message.Chat.Id, "к такому я не был готов");
+					//            }
+					//            else
+					//            {
+					//                var t = await bot.SendTextMessage(update.Message.Chat.Id, ansver);
+					//            }
 
-                    //            Console.WriteLine(responseString);
+					//        }
 
-                    //            Currency currency = new Currency(responseString);
+					//        Console.WriteLine("Echo Message: {0}", update.Message.Text);
+					//    }
+					//    else if (inputMessage.StartsWith("/currency"))
+					//    {
+					//        var messageParts = inputMessage.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+					//        var kindCurrency = messageParts.Length == 1 ? "USDBYR" : messageParts.Skip(1).First();
+					//        WebUtility.UrlEncode(kindCurrency);
 
-                    //            Console.WriteLine(string.Format("{0} is: {1} ", currency.Id, currency.Rate));
+					//        string url =
+					//            string.Format(
+					//                "https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22{0}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=",
+					//                kindCurrency);
+					//        WebRequest request = WebRequest.Create(url);
+					//        WebResponse response = request.GetResponse();
+					//        using (var streamReader = new StreamReader(response.GetResponseStream()))
+					//        {
+					//            string responseString = streamReader.ReadToEnd();
 
-                    //            var message = "Курс " + currency.Name + " Дата " + currency.Date + " Время " +
-                    //                          currency.Time + " Покупка " + currency.Ask + " Продажа " +
-                    //                          currency.Bid;
+					//            Console.WriteLine(responseString);
 
-                    //            var t = await bot.SendTextMessage(update.Message.Chat.Id, message);
-                    //            Console.WriteLine("Echo Message: {0}", message);
-                    //        }
-                    //    }
-                    //    else if (inputMessage.StartsWith("/weather"))
-                    //    {
-                    //        var messageParts = inputMessage.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                    //        var city = messageParts.Length == 1 ? "Minsk" : messageParts.Skip(1).First();
-                    //        WebUtility.UrlEncode(city);
-                    //        var weatherApiKey = "ec259b32688dc1c1d087ebc30cbff9ed";
-                    //        string url =
-                    //            string.Format(
-                    //                "http://api.openweathermap.org/data/2.5/weather?q={0}&APPID={1}&units=metric",
-                    //                city, weatherApiKey);
-                    //        WebRequest request = WebRequest.Create(url);
-                    //        WebResponse response = request.GetResponse();
-                    //        using (var streamReader = new StreamReader(response.GetResponseStream()))
-                    //        {
-                    //            string responseString = streamReader.ReadToEnd();
+					//            Currency currency = new Currency(responseString);
 
-                    //            Console.WriteLine(responseString);
-                    //            JObject joResponse = JObject.Parse(responseString);
-                    //            JObject main = (JObject)joResponse["main"];
-                    //            double temp = (double)main["temp"];
-                    //            JObject weather = (JObject)joResponse["weather"][0];
-                    //            string description = (string)weather["description"];
-                    //            string cityName = (string)joResponse["name"];
+					//            Console.WriteLine(string.Format("{0} is: {1} ", currency.Id, currency.Rate));
 
-                    //            Console.WriteLine(string.Format("temp is: {0}", temp));
+					//            var message = "Курс " + currency.Name + " Дата " + currency.Date + " Время " +
+					//                          currency.Time + " Покупка " + currency.Ask + " Продажа " +
+					//                          currency.Bid;
 
-                    //            var message = "In " + cityName + " " + description + " and the temperature is " +
-                    //                          temp.ToString("+#;-#") + "°C";
+					//            var t = await bot.SendTextMessage(update.Message.Chat.Id, message);
+					//            Console.WriteLine("Echo Message: {0}", message);
+					//        }
+					//    }
+					//    else if (inputMessage.StartsWith("/weather"))
+					//    {
+					//        var messageParts = inputMessage.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+					//        var city = messageParts.Length == 1 ? "Minsk" : messageParts.Skip(1).First();
+					//        WebUtility.UrlEncode(city);
+					//        var weatherApiKey = "ec259b32688dc1c1d087ebc30cbff9ed";
+					//        string url =
+					//            string.Format(
+					//                "http://api.openweathermap.org/data/2.5/weather?q={0}&APPID={1}&units=metric",
+					//                city, weatherApiKey);
+					//        WebRequest request = WebRequest.Create(url);
+					//        WebResponse response = request.GetResponse();
+					//        using (var streamReader = new StreamReader(response.GetResponseStream()))
+					//        {
+					//            string responseString = streamReader.ReadToEnd();
 
-                    //            var t = await bot.SendTextMessage(update.Message.Chat.Id, message);
-                    //            Console.WriteLine("Echo Message: {0}", message);
-                    //        }
-                    //    }
-                    //    else
-                    //    {
-                    //        await bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
-                    //        await Task.Delay(2000);
-                    //        var t = await bot.SendTextMessage(update.Message.Chat.Id, update.Message.Text);
-                    //        Console.WriteLine("Echo Message: {0}", update.Message.Text);
-                    //    }
-                    //}
+					//            Console.WriteLine(responseString);
+					//            JObject joResponse = JObject.Parse(responseString);
+					//            JObject main = (JObject)joResponse["main"];
+					//            double temp = (double)main["temp"];
+					//            JObject weather = (JObject)joResponse["weather"][0];
+					//            string description = (string)weather["description"];
+					//            string cityName = (string)joResponse["name"];
 
-                    offset = update.Id + 1;
-                }
+					//            Console.WriteLine(string.Format("temp is: {0}", temp));
 
-                await Task.Delay(1000);
-            }
-        }
-    }
+					//            var message = "In " + cityName + " " + description + " and the temperature is " +
+					//                          temp.ToString("+#;-#") + "°C";
+
+					//            var t = await bot.SendTextMessage(update.Message.Chat.Id, message);
+					//            Console.WriteLine("Echo Message: {0}", message);
+					//        }
+					//    }
+					//    else
+					//    {
+					//        await bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
+					//        await Task.Delay(2000);
+					//        var t = await bot.SendTextMessage(update.Message.Chat.Id, update.Message.Text);
+					//        Console.WriteLine("Echo Message: {0}", update.Message.Text);
+					//    }
+					//}
+
+					offset = update.Id + 1;
+				}
+
+				await Task.Delay(1000);
+			}
+		}
+	}
 }
--- become-solid-in-csharp-master/day-1/DBCreationProject/Program.cs
+++ become-solid-in-csharp-master/day-1/DBCreationProject/Program.cs
@@ -8,43 +8,44 @@ using DBCreationProject.Models;
 
 namespace DBCreationProject
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            using (AnsverContex db = new AnsverContex())
-            {
-                // создаем два объекта User 
-                Answer a1 = new Answer { Value= "привет"};
-                Answer a2 = new Answer { Value = "меня зовут Тимоша" };
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try {
+				using (AnsverContex db = new AnsverContex()) {
+					// создаем два объекта User 
+					Answer a1 = new Answer { Value = "привет" };
+					Answer a2 = new Answer { Value = "меня зовут Тимоша" };
 
-                Question q1 = new Question { Value = "привет" };
-                Question q2 = new Question { Value = "как тебя зовут" };
+					Question q1 = new Question { Value = "привет" };
+					Question q2 = new Question { Value = "как тебя зовут" };
 
-                // добавляем их в бд 
-                db.Ansvers.Add(a1);
-                db.Ansvers.Add(a2);
-                db.Questions.Add(q1);
-                db.Questions.Add(q2);
-                db.SaveChanges();
-                Console.WriteLine("Объекты успешно сохранены");
+					// добавляем их в бд 
+					db.Ansvers.Add(a1);
+					db.Ansvers.Add(a2);
+					db.Questions.Add(q1);
+					db.Questions.Add(q2);
+					db.SaveChanges();
+					Console.WriteLine("Объекты успешно сохранены");
 
-                // получаем объекты из бд и выводим на консоль 
-                var ansvers = db.Ansvers;
-                var questions = db.Questions;
-                Console.WriteLine("Список объектов:");
-                foreach (Answer ansver in ansvers)
-                {
-                    Console.WriteLine("{0}.{1}", ansver.Id, ansver.Value);
-                }
-                foreach (Question question in questions)
-                {
-                    Console.WriteLine("{0}.{1}", question.Id, question.Value);
-                }
-            }
-            Console.Read();
-        }
+					// получаем объекты из бд и выводим на консоль 
+					var ansvers = db.Ansvers;
+					var questions = db.Questions;
+					Console.WriteLine("Список объектов:");
+					foreach (Answer ansver in ansvers) {
+						Console.WriteLine("{0}.{1}", ansver.Id, ansver.Value);
+					}
+					foreach (Question question in questions) {
+						Console.WriteLine("{0}.{1}", question.Id, question.Value);
+					}
+				}
+				Console.Read();
+			}
+			catch(Exception ex) {
 
-    }
+			}
+		}
+	}
 }
 
